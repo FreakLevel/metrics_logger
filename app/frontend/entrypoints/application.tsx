b6b5929df27d02077ab764a3e5c1b7a35a8ed8fe
Eduardo Tapia <eduardo.tapiam97@gmail.com>
2022-07-10 03:11:33 -0400

    feat(front): add react and vite

---
+++ app/frontend/entrypoints/application.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+render(
+  <React.StrictMode>
+    <div>Hello, world!</div>
+  </React.StrictMode>,
+  document.getElementById('root')
+)
